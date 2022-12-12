3745192e539ffa354159a03bc91f41a5cceb971b
Bojan <bojan.savic@smail.inf.h-brs.de>
2022-12-12 18:47:35 +0100

    enabled https

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express')
+const https = require('https');
+const fs = require('fs');
+
 const app = express()
-const port = 3000
+const httpPort = 3000
+const httpsPort = 3001
+
+let key = fs.readFileSync(__dirname + '/../certs/selfsigned.key');
+let cert = fs.readFileSync(__dirname + '/../certs/selfsigned.crt');
+let options = {
+    key: key,
+    cert: cert
+};
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(httpPort, () => {
+    console.log(`Example app listening on port ${httpPort}`)
 })
+
+let server = https.createServer(options, app);
+
+server.listen(httpsPort, () => {
+    console.log("https server starting on port : " + httpsPort)
+});
