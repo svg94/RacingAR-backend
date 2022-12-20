d7a928df7cddee3ab2468f06740e3b908c2413b4
Bojan <bojan.savic@smail.inf.h-brs.de>
2022-12-20 13:46:57 +0100

    Zwischenstand socket verbindung

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
+const cors = require('cors');
+const { Server } = require("socket.io");
+
 
 const app = express();
+app.use(cors())
 const httpPort = 3000;
 const httpsPort = 3001;
+
+app.use(function(req,res,next){
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+});
 
 let key = fs.readFileSync(__dirname + '/certs/client-key.pem');
 let cert = fs.readFileSync(__dirname + '/certs/client-cert.pem');
 let options = {
     key: key,
-    cert: cert
+    cert: cert,
+    requestCert: false,
+    rejectUnauthorized: false
 };
 
 app.get('/', (req, res) => {
@@ -21,6 +32,14 @@ app.listen(httpPort, () => {
 });
 
 let server = https.createServer(options, app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 
 server.listen(httpsPort, () => {
     console.log("https server starting on port : " + httpsPort);
