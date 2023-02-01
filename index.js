bf8b9099495d3ec41df3a930d7fc5c215c08ff42
BSavic <bsavic@conet.de>
2023-02-01 21:13:35 +0100

    finally fixed socket issues

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
-const cors = require('cors');
 const { Server } = require("socket.io");
 
 
 const app = express();
-app.use(cors())
 const httpPort = 3000;
 const httpsPort = 3001;
-
-app.use(function(req,res,next){
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
-});
 
 let key = fs.readFileSync(__dirname + '/certs/client-key.pem');
 let cert = fs.readFileSync(__dirname + '/certs/client-cert.pem');
